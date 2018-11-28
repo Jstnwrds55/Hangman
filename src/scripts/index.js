4131f2457bf217fb25ba611d66efdb513c290091
Justin <jstnwrds55@gmail.com>
2018-11-28 12:20:48 -0700

    game flow working properly

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,10 +16,93 @@ var randomWords = require("random-word-by-length");
 var randomCountry = require("random-country");
 
 // Game variables
-let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    wrongUserGuesses = 0,
-    correctLetters = 0;
+let alphabet,
+    wrongUserGuesses,
+    correctLetters;
 
+function gameGeneration() {
+  alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  wrongUserGuesses = 0,
+  correctLetters = 0;
+
+  // display game
+  introDiv.style.display = "none";
+  gameButton.style.display = "none";
+  hangerDiv.style.display = "";
+  guessingDiv.style.display = "";
+
+  // alphabet generation
+  lettersDiv.innerHTML = "";
+  for (let letter in alphabet) {
+    letters.getElementsByTagName("p")[0].innerText = alphabet[letter];
+    lettersDiv.innerHTML += letters.innerHTML;
+  };
+
+  // generate word to guess and set answerDiv to nothing
+  let wordToGuess = randomWords();
+  answersDiv.innerHTML = "";
+  console.log(wordToGuess);
+
+  // MAIN GAME DRIVER
+  // alphabet click action generation
+  for (let letter in alphabet) {
+    let letterToClick = document.getElementsByClassName("letter-box")[letter];
+    letterToClick.addEventListener("click", function() {
+      let correctAnswer = false;
+      // loop through word to guess and see if it matches
+      for (let index = 0; index < wordToGuess.length; index++) {
+
+        // if the letter in the word to guess is equal to the one guessed, show it
+        if (wordToGuess.charAt(index).toUpperCase() == letterToClick.innerText.charAt(0).toUpperCase()) {
+          answersDiv.getElementsByTagName("p")[index].innerText = wordToGuess.charAt(index);
+          answersDiv.getElementsByTagName("p")[index].style.textIndent = "0px";
+          correctLetters++;
+          correctAnswer = true;
+        } else {
+          
+        }
+      }
+
+      // user guesses wrong
+      if (!correctAnswer) {
+        // add another guess if the type of is a string meaning it hasn't been guessed
+        if (typeof(alphabet[letter]) === "string") {
+          wrongUserGuesses++; 
+          if (wrongUserGuesses < 9) {
+            guessesRemaining.innerHTML = "Incorrect<br />Guesses<br />Remaining:<br />" +(8 - wrongUserGuesses);
+          }
+        }
+      } else if (correctLetters == parseInt(wordToGuess.length)) {
+        resultsDiv.getElementsByTagName("p")[0].innerText = "Congratulations, you guessed it! The word was: ";
+        guessingDiv.style.display = "none";
+        resultsDiv.getElementsByTagName("h2")[0].innerText = wordToGuess;
+        resultsDiv.style.display = "";
+        console.log("winner!");
+      }
+
+      alphabet[letter] = 0; // zero out spot in alphabet
+
+      // user losing scenario
+      if (wrongUserGuesses > 8) {
+        guessingDiv.style.display = "none";
+        resultsDiv.getElementsByTagName("p")[0].innerText = "Awwww, you took too many guesses! The word was: ";
+        resultsDiv.getElementsByTagName("h2")[0].innerText = wordToGuess;
+        resultsDiv.style.display = "inline-block";
+        console.log("loser!");
+      }
+
+      letterToClick.style.opacity = ".2";
+
+    });
+  }
+
+  // Generate answerDiv with placeholder letters and shift the letters
+  for (let i = 0; i < wordToGuess.length; i++) {
+    answersLetters.getElementsByTagName("p")[0].innerText = "A";
+    answersDiv.innerHTML += answersLetters.innerHTML;
+    answersDiv.getElementsByTagName("p")[i].style.textIndent = "-9999px";
+  }
+}
 
 // DOM Variables
 let gameButton = document.getElementById("game-button"),
@@ -42,86 +125,13 @@ guessingDiv.style.display = "none";
 
 // make the play game button do something
 gameButton.addEventListener("click", function() {
-  introDiv.style.display = "none";
-  gameButton.style.display = "none";
-  hangerDiv.style.display = "";
-  guessingDiv.style.display = "";
+  gameGeneration();
 });
-
-// alphabet generation
-lettersDiv.innerHTML = "";
-for (let letter in alphabet) {
-  letters.getElementsByTagName("p")[0].innerText = alphabet[letter];
-  lettersDiv.innerHTML += letters.innerHTML;
-};
-
-// generate word to guess and set answerDiv to nothing
-let wordToGuess = randomWords();
-answersDiv.innerHTML = "";
- console.log(wordToGuess);
-
-// MAIN GAME DRIVER
-// alphabet click action generation
-for (let letter in alphabet) {
-  let letterToClick = document.getElementsByClassName("letter-box")[letter];
-  letterToClick.addEventListener("click", function() {
-    let correctAnswer = false;
-    // loop through word to guess and see if it matches
-    for (let index = 0; index < wordToGuess.length; index++) {
-
-      // if the letter in the word to guess is equal to the one guessed, show it
-      if (wordToGuess.charAt(index).toUpperCase() == letterToClick.innerText.charAt(0).toUpperCase()) {
-        answersDiv.getElementsByTagName("p")[index].innerText = wordToGuess.charAt(index);
-        answersDiv.getElementsByTagName("p")[index].style.textIndent = "0px";
-        correctLetters++;
-        correctAnswer = true;
-      } else {
-        
-      }
-    }
-
-    // user guesses wrong
-    if (!correctAnswer) {
-      // add another guess if the type of is a string meaning it hasn't been guessed
-      if (typeof(alphabet[letter]) === "string") {
-        wrongUserGuesses++; 
-        if (wrongUserGuesses < 9) {
-          guessesRemaining.innerHTML = "Incorrect<br />Guesses<br />Remaining:<br />" +(8 - wrongUserGuesses);
-        }
-      }
-    } else if (correctLetters == parseInt(wordToGuess.length)) {
-      guessingDiv.style.display = "none";
-      resultsDiv.getElementsByTagName("h2")[0].innerText = wordToGuess;
-      resultsDiv.style.display = "";
-      console.log("winner!");
-    }
-
-    alphabet[letter] = 0; // zero out spot in alphabet
-
-    // user losing scenario
-    if (wrongUserGuesses > 8) {
-      guessingDiv.style.display = "none";
-      resultsDiv.getElementsByTagName("p")[0].innerText = "Awwww, you took too many guesses! The word was: ";
-      resultsDiv.getElementsByTagName("h2")[0].innerText = wordToGuess;
-      resultsDiv.style.display = "inline-block";
-      console.log("loser!");
-    }
-
-    letterToClick.style.opacity = ".2";
-
-  });
-}
-
-// Generate answerDiv with placeholder letters and shift the letters
-for (let i = 0; i < wordToGuess.length; i++) {
-  answersLetters.getElementsByTagName("p")[0].innerText = "A";
-  answersDiv.innerHTML += answersLetters.innerHTML;
-  answersDiv.getElementsByTagName("p")[i].style.textIndent = "-9999px";
-}
 
 // play again button
 playAgainButton.addEventListener("click", function() {
-
+  resultsDiv.style.display = "none";
+  gameGeneration();
 });
 
 function generateCountry() {
